48da5ff6b7ac5137a30e3e20bd8069e16594061a
jreiter <jgreit@gmail.com>
2015-07-23 21:45:34 -0400

    Further improvement on subclonal detection.

--- src/treeomics/__main__.py
+++ src/treeomics/__main__.py
@@ -353,8 +353,8 @@ def main():
 
             # determine mutation patterns based on standard binary classification to generate an overview graph
 
-            phylogeny = ti.create_max_lh_tree(os.path.join(output_directory, 'mlhtree_'+fn_pattern+'.tex'),
-                                              patient)
+            phylogeny = ti.create_max_lh_tree(os.path.join(output_directory, 'mlhtree_'+fn_pattern+'.tex'), patient,
+                                              min_mp_lh=settings.MIN_MP_LH if 0 < settings.MIN_MP_LH < 1 else None)
 
             # determine mutation patterns based on standard binary classification to generate an overview graph
             if plots_report:
